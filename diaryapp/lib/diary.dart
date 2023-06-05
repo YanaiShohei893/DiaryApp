cec19cc3c93a88232b8b757930a629113346a6ef
yanai shohei <zeal20410026@fsgcl.jp>
2023-06-05 09:23:25 +0900

    '0530作業内容'

--- diaryapp/lib/diary.dart
+++ diaryapp/lib/diary.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'diary_detail.dart';
 
-  DateTime now = DateTime.now();
-  int _year = now.year;
-  int _month = now.month;
-  int _day = now.day;
-
 //日記一覧画面
 class DiaryPage extends StatefulWidget {
   @override
@@ -13,8 +8,9 @@ class DiaryPage extends StatefulWidget {
 }
 
 class _DiaryPageState extends State<DiaryPage> {
-  List<String> diaryes = [
-  ];
+  List<String> diaryes = [];
+  List<int> M = [];
+  List<int> D = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar( 
@@ -22,39 +18,69 @@ class _DiaryPageState extends State<DiaryPage> {
       leading: Text("button"),
       centerTitle: true,
       title: Text("Diary"),
-      actions: [Text("action1"),Text("action2")],
+      actions: [
+        Text("action1"),
+        IconButton(
+          icon: Icon(Icons.check_circle),
+          onPressed:() {}
+        ),
+        ],
     ),
     body: ListView.builder(
       itemCount: diaryes.length,
-      itemBuilder: (context, index) {
+      itemBuilder: (context,index) {
         return Card(
-          child:ListTile(
-            leading: Text('年/月'),
-            title: Text('日'),
-            subtitle: Text(diaryes[index]),
-            onLongPress: (){
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => diary_DetailPage()
-                )
-              );
-            },
-          )
-        );
-      }
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => diary_DetailPage()
-          )
+          child: ListTile(
+            leading: Text(M[index].toString() + '/' ),
+            title: Text(diaryes[index]),
+            trailing: IconButton(
+              onPressed:(){
+                setState(() {
+                  diaryes.removeAt(index);
+                  M.removeAt(index);
+                });
+              },
+              icon: Icon(Icons.delete),
+            )
+          ),
         );
       },
+    ),
+    // body: ListView.builder(
+    //   itemCount: diaryes.length,
+    //   itemBuilder: (context, index) {
+    //     return Card(
+    //       child:ListTile(
+    //         leading: Text('年/月'),
+    //         title: Text('日'),
+    //         subtitle: Text(diaryes[index]),
+    //         onLongPress: (){
+    //           Navigator.push(
+    //             context,
+    //             MaterialPageRoute(
+    //               builder: (context) => diary_DetailPage()
+    //             )
+    //           );
+    //         },
+    //       )
+    //     );
+    //   }
+    floatingActionButton: FloatingActionButton(
+      onPressed: () async {
+        final newListText = await Navigator.of(context).push(
+          MaterialPageRoute(builder:(context) {
+            return diary_DetailPage();
+          }),
+        );
+        if (newListText != null) {
+          setState(() {
+            diaryes.add(newListText);
+            M.add(newListText);
+          });
+        }
+      },
       child: Icon(Icons.history_edu)
-    )
+    ),
     );
 
     //  body:ListView(
--- diaryapp/lib/diary_detail.dart
+++ diaryapp/lib/diary_detail.dart
@@ -1,29 +1,53 @@
 import 'package:flutter/material.dart';
 
-class diary_DetailPage extends StatelessWidget {
+class diary_DetailPage extends StatefulWidget {
+  @override
+  _diary_DetailPage createState() => _diary_DetailPage();
+}
+
+class _diary_DetailPage extends State<diary_DetailPage> {
+  String _text = '';
+  int _month = 0;
   @override
   Widget build(BuildContext context) {
     DateTime now = DateTime.now();
-    int _year = now.year;
-    int _month = now.month;
-    int _day = now.day;
- 
+    int year = now.year;
+    int month = now.month;
+    int day = now.day;
     return Scaffold(appBar: AppBar( 
       backgroundColor: Color.fromARGB(82, 99, 224, 222),
       leading: BackButton(),
       centerTitle: true,
-      title: Text("$_month/$_day"),
-      actions: [Text("action1"),Text("action2")],
+      title: Text("$month/$day"),
+      actions: [
+      ElevatedButton(
+        onPressed: _text.length == 0 ? null : (){
+          Navigator.of(context).pop(_text);
+          Navigator.of(context).pop(_month);
+          // Navigator.of(context).pop(_day);
+        },
+        child:Text('保存')
+      ),
+      ],
     ),
     body: Container(
       padding: EdgeInsets.all(52),
       child:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children:<Widget>[
           TextField(
+            onChanged: (String value) {
+              setState(() {
+                _text = value;
+                _month = month;
+              });
+            },
             keyboardType: TextInputType.multiline,
             maxLines: null,
-          ),
-        ],))
+          ),            
+        ],
+        ),
+        ),
     );
   }
 }
